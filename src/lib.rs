9d9f06c8efbc7f0610b9a33fb54b5c517e0d1edb
Brookzerker <brooks@brookzerker.com>
2025-12-03 11:28:14 -0800

    supporting paths now, and type works with paths

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,32 @@
-mod commands;
+mod builtin_commands;
 mod errors;
 pub mod utilities;
 
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::os::unix::ffi::OsStrExt;
 
 use anyhow::{Context, Result};
 
 use crate::{
-    commands::{Command, builtin_type::builtin_type, echo::echo},
+    builtin_commands::{BuiltinCommand, builtin_type::builtin_type, echo::echo},
     errors::CustomError,
-    utilities::{get_command, print_error, print_prompt},
+    utilities::{get_command, get_path, print_error, print_prompt},
 };
 
 pub fn run() -> Result<()> {
+    let path = get_path().context("Getting path")?;
+
     loop {
         print_prompt();
 
         let command = get_command().context("getting command")?;
 
         match command {
-            Command::Echo(command_string) => echo(&command_string),
-            Command::Exit => break,
-            Command::Type(arguments) => builtin_type(arguments),
-            Command::NotFound(command_string) => {
+            BuiltinCommand::Echo(command_string) => echo(command_string.as_slice()),
+            BuiltinCommand::Exit => break,
+            BuiltinCommand::Type(arguments) => builtin_type(arguments, &path),
+            BuiltinCommand::NotFound(command_string) => {
                 let error = CustomError::CommandNotFound(command_string);
                 print_error(error);
             }
--- src/utilities.rs
+++ src/utilities.rs
@@ -1,9 +1,12 @@
-use crate::commands::Command;
-use anyhow::{Context, Result};
+use crate::builtin_commands::{BuiltinCommand, echo::echo};
+use anyhow::{Context, Error, Result, anyhow, bail};
+use core::error;
 pub use std::process::exit;
 use std::{
+    env::{self, SplitPaths, split_paths},
     fmt::Display,
-    io::{self, Write, stdin},
+    io::{self, ErrorKind, Write, stdin},
+    path::{Path, PathBuf},
 };
 
 pub fn get_user_input() -> Result<String> {
@@ -23,14 +26,27 @@ pub fn print_prompt() {
     io::stdout().flush().unwrap();
 }
 
-pub fn get_command() -> Result<Command> {
+pub fn get_command() -> Result<BuiltinCommand> {
     let user_input = get_user_input()?;
     let mut split_user_input = user_input.split_whitespace();
     let command_input = split_user_input.next().unwrap_or(" ").to_owned();
     let arguments = split_user_input
         .map(ToOwned::to_owned)
         .collect::<Vec<String>>();
-    let command = Command::from((command_input, arguments));
+    let command = BuiltinCommand::from((command_input, arguments));
 
     Ok(command)
+}
+
+pub fn get_path() -> Result<Vec<PathBuf>> {
+    let path = env::var("PATH").context("Getting PATH environment variable")?;
+    let split_paths = split_paths(&path).map(|path| {
+        if path.is_file() {
+            bail!("PATH from environment variable is an file")
+        } else {
+            Ok(path)
+        }
+    });
+
+    split_paths.collect()
 }
