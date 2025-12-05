1e6c63076dd828f79b0edf98db01c4c194bf7049
Brookzerker <brooks@brookzerker.com>
2025-12-05 09:48:10 -0800

    implemented ~ in change directory

--- src/builtin_commands/change_directory.rs
+++ src/builtin_commands/change_directory.rs
@@ -7,16 +7,17 @@ use std::{
 use crate::utilities::print_error;
 
 pub fn change_directory(arguments: &[String]) -> Result<()> {
+    let Some(home_directory) = home_dir() else {
+        bail!("Error: you don't seem to have a home directory");
+    };
     let Some(target_path) = arguments.first() else {
-        let Some(home_directory) = home_dir() else {
-            bail!("Error: you don't seem to have a home directory");
-        };
-
         std::env::set_current_dir(home_directory).context("changing to home directory")?;
         return Ok(());
     };
 
-    let target_path = Path::new(target_path);
+    let target_path = target_path.replace("~", home_directory.to_str().unwrap_or_default());
+
+    let target_path = Path::new(&target_path);
 
     if target_path.is_dir() {
         set_current_dir(target_path).context("Changing to target directory")?;
